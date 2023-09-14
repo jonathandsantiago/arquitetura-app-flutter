07e05e9c83267f85b279cff04f5bc6a62608b91c
Jonathan Santiago <jonathandcsantiago@outlook.com>
2023-09-13 22:10:19 -0400

    feat: criado uma estrutura base para iniciar um projeto em flutter com go_router e get_it

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_localization/flutter_localization_plugin_c_api.h>
+#include <geolocator_windows/geolocator_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterLocalizationPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLocalizationPluginCApi"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
+}
